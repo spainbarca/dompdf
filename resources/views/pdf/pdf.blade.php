3fa123cbc9c2a9b8d34a8deb769761631fd2939b
Noah <spain.barcelona.1999@gmail.com>
2023-10-13 22:58:51 -0500

    Estilos css

--- resources/views/pdf/pdf.blade.php
+++ resources/views/pdf/pdf.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>{{ $titulo }}</title>
+    <link rel="stylesheet" href="{{ public_path('css/pdf.css') }}">
 </head>
 <body>
     <h1>{{ $contenido }}</h1>
@@ -10,8 +11,9 @@
         <img src="https://img.freepik.com/vector-gratis/fondo-blanco-gris-brillante-lineas-onduladas_1017-25101.jpg" alt="">
         <hr>
         <img src={{ public_path('img/kings-league.png') }} alt="">
-        <h2>Kings League</h2>
-        <p>La Kings League, denominada oficialmente por motivos de patrocinio como Kings League InfoJobs, es una liga de fútbol 7 con sede en Barcelona, España, creada por el exfutbolista Gerard Piqué en 2022 en asociación con otras personalidades de internet y streamers.</p>
+        <h2 class="titulo">Kings League</h2>
+        <p class="p">La Kings League, denominada oficialmente por motivos de patrocinio como Kings League InfoJobs, es una liga de fútbol 7 con sede en Barcelona, España, creada por el exfutbolista Gerard Piqué en 2022 en asociación con otras personalidades de internet y streamers.</p>
+        <button>Vamos!!</button>
     </div>
 </body>
 </html>
